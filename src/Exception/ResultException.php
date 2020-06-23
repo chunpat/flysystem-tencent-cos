9fe31e86d5142ac2e5b32352d7d2721ea10d34a8
chunpat cheung <398949389@qq.com>
2020-06-23 13:06:45 +0000

    Apply fixes from StyleCI

--- src/Exception/ResultException.php
+++ src/Exception/ResultException.php
@@ -3,13 +3,11 @@
  * Created by PhpStorm.
  * User: zzhpeng
  * Date: 20/6/2020
- * Time: 7:54 AM
+ * Time: 7:54 AM.
  */
 
 namespace Chunpat\FlysystemTencentCos\Exception;
 
-
 class ResultException extends \Exception
 {
-
 }
